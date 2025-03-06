5d501a126c17d10267039b5b6cda7c42ad171546
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2025-03-05 18:39:52 -0800

    Introduce O2014 and basic tests

--- rbamlib/models/dll/O2014.py
+++ rbamlib/models/dll/O2014.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def O2014():
+def O2014(L, kp, dll_type='M'):
     r"""
     Calculate radial diffusion coefficient following Ozeke et al. (2014) [#]_ model.
 
@@ -9,16 +9,54 @@ def O2014():
 
     Parameters
     ----------
+    kp : ndarray
+        Kp-index, vector of geomagnetic activity indices corresponding to the times.
+    L : ndarray
+        Array of L values, representing the radial distance in Earth radii where magnetic field lines cross the magnetic equator.
+    dll_type : str, default='M'
+        A string, indicating which diffusion coefficient to calculate.
+        'M' or 'B': stands for magnetic radial diffusion coefficient (eq. 20).
+        'E' stands for electric diffusion coefficient (eq. 23).
+        'ME', 'BE', 'both', or '' computes both coefficients and returns a tuple (dllm, dlle).
+
 
     Returns
     -------
+    dllm : numpy.ndarray
+        Electromagnetic radial diffusion coefficient, in 1/days (if dll_type='M').
+    dlle : numpy.ndarray
+        Electrostatic radial diffusion coefficient, in 1/days (if dll_type='E').
+    dllm, dlle : tuple of numpy.ndarray
+        Both electromagnetic and electrostatic diffusion coefficients (dllm, dlle) if dll_type='ME' or ''.
 
     Notes
     -----
+    The electromagnetic radial diffusion coefficient is calculated as:
+
     .. math::
+        D^{B}_{LL} = 6.62 \times 10^{-13} L^8 10^{-0.0327 L^2 + 0.625 L - 0.0108 K_p^2 + 0.499 K_p}
+
+    The electrostatic radial diffusion coefficient is calculated as:
+
+    .. math::
+        D^{E}_{LL} = 2.16 \times 10^{-8} L^6 10^{0.217 L + 0.461 K_p}
 
     References
     ----------
     .. [#] Ozeke, L. G., Mann, I. R., Murphy, K. R., Jonathan Rae, I., & Milling, D. K. (2014). Analytic expressions for ULF wave radiation belt radial diffusion coefficients. Journal of Geophysical Research, [Space Physics], 119(3), 1587–1605. https://doi.org/10.1002/2013JA019204
     """
-    return
+    dllm, dlle = None, None
+
+    if dll_type in {'M', 'm', '', 'ME', 'me', 'both', 'B', 'b', 'BE', 'be'}:
+        dllm = 6.62e-13 * L ** 8 * 10 ** (-0.0327 * L ** 2 + 0.625 * L - 0.0108 * kp ** 2 + 0.499 * kp)
+
+    if dll_type in {'E', 'e', '', 'ME', 'me', 'both', 'BE', 'be'}:
+        dlle = 2.16e-8 * L ** 6 * 10 ** (0.217 * L + 0.461 * kp)
+
+    if dll_type in {'M', 'm', 'B', 'b'}:
+        return dllm
+    elif dll_type in {'E', 'e'}:
+        return dlle
+    elif dll_type in {'ME', 'me', 'both', '', 'BE', 'be'}:
+        return dllm, dlle
+
