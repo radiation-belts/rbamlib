147c4fc20333a8d89301af65c3037296d23b9fce
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2025-03-05 17:40:54 -0800

    Cleanup and testing of BA model

--- tests/models/test_dll.py
+++ tests/models/test_dll.py
@@ -16,21 +16,21 @@ class TestDll(unittest.TestCase, TestHelpers):
         """Test BA2000 values for all diffusion coefficient types."""
         # Test electromagnetic diffusion coefficient
         dllm = BA2000(self.L, self.kp, dll_type='M')
-        expected_dllm = np.array([0.00510035, 0.0475007, 0.29411184])
-        np.testing.assert_almost_equal(dllm, expected_dllm, decimal=4,
+        expected_dllm = np.array([0.00510035, 0.0475007, 0.29411184]) # From matlab
+        np.testing.assert_almost_equal(dllm, expected_dllm, decimal=3,
                                        err_msg="Electromagnetic diffusion coefficients are incorrect.")
 
         # Test electrostatic diffusion coefficient
         dlle = BA2000(self.L, self.kp, mu=self.mu, dll_type='E')
-        expected_dlle = np.array([8.4195e-03, 4.3609e-02, 1.7471e-01])
-        np.testing.assert_almost_equal(dlle, expected_dlle, decimal=4,
+        expected_dlle = np.array([8.4195e-03, 4.3609e-02, 1.7471e-01]) # From matlab
+        np.testing.assert_almost_equal(dlle, expected_dlle, decimal=3,
                                        err_msg="Electromagnetic diffusion coefficients are incorrect.")
 
         # Test both diffusion coefficients
         dllm_both, dlle_both = BA2000(self.L, self.kp, mu=self.mu, dll_type='ME')
-        np.testing.assert_almost_equal(dllm_both, expected_dllm, decimal=4,
+        np.testing.assert_almost_equal(dllm_both, expected_dllm, decimal=3,
                                        err_msg="Electromagnetic coefficients from 'ME' type are incorrect.")
-        np.testing.assert_almost_equal(dlle_both, expected_dlle, decimal=4,
+        np.testing.assert_almost_equal(dlle_both, expected_dlle, decimal=3,
                                        err_msg="Electrostatic coefficients from 'ME' type are incorrect.")
 
         # Test error for missing mu
