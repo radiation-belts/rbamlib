88efa9068e9f5a3350c16f0f5efa91c3c39d220d
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2025-03-05 17:38:05 -0800

    Adding additional constants and tests of constants

--- rbamlib/constants.py
+++ rbamlib/constants.py
@@ -1,5 +1,5 @@
 """
-Physical constants for use in `rbamlib` calculations.
+Physical constants for use in `rbamlib` calculations. Most of the constants are defined in CGS system with a few exceptions.
 
 Constants:
     - MC2 ≈ 0.511 : Rest mass energy of an electron in MeV.
@@ -8,7 +8,20 @@ Constants:
     - B0_Jupiter ≈ 4.28 : mean value of the magnetic field at the magnetic equator on the Jupiter's surface in G.
     - T0 ≈ 1.3802 : Approximate value of T(0)
     - T1 ≈ 0.7405 : Approximate value of T(1)
+    - R_Earth ≈ 637,800,000 : Radius of Earth in cm
+    - R_Saturn ≈ 6,026,800,000 : Radius of Earth in cm
+    - R_Jupiter ≈ 7,149,200,000 : Radius of Earth in cm
+    - c ≈ 2.9e10 : Speed of light in cm/s
+    - q ≈ 4.8e-10 : electron charge in CGS
+
+CGS:
+    - Class of constants in CGS
+
+SI:
+    - Class of constants in SI
 """
+
+from scipy import constants as scic
 
 MC2 = 0.51099895  # rest mass energy of an electron in MeV
 B0_Earth = 0.312  # mean value of the Earth's surface magnetic field in Gauss
@@ -16,6 +29,34 @@ B0_Saturn = 0.215  # mean value of the Saturn's surface magnetic field in Gauss
 B0_Jupiter = 4.28  # mean value of the Jupiter's surface magnetic field in Gauss
 T0 = 1.3802  # Approximate value of T(0)
 T1 = 0.7405  # Approximate value of T(1)
-R_Earth = 6.356e8  # Earth radius in cm
-c = 2.99792458e10  # Speed of light in cm/s
+
+# Based on NASA Planetary Fact Sheet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+R_Earth = 6.378e8  # Earth radius in cm
+R_Saturn = 60.268e8  # Saturn radius in cm
+R_Jupiter = 71.492e8  # Jupiter radius in cm
+c = scic.c * 100  # Speed of light in cm/s
 q = 4.803e-10  # charge of electron in CGS
+
+
+class CGS:
+    MC2 = MC2 * 1e6 * scic.eV / scic.erg  # erg (electron rest energy)
+    B0_Earth = B0_Earth
+    B0_Saturn = B0_Saturn
+    B0_Jupiter = B0_Jupiter
+    R_Earth = R_Earth
+    R_Saturn = R_Saturn
+    R_Jupiter = R_Jupiter
+    c = c
+    q = q
+
+
+class SI:
+    MC2 = CGS.MC2 * scic.erg  # Joules (1 erg = 1e-7 J)
+    B0_Earth = CGS.B0_Earth * 1e-4  # Tesla
+    B0_Saturn = CGS.B0_Saturn * 1e-4  # Tesla
+    B0_Jupiter = CGS.B0_Jupiter * 1e-4  # Tesla
+    R_Earth = CGS.R_Earth * 1e-2  # meters
+    R_Saturn = CGS.R_Saturn * 1e-2  # meters
+    R_Jupiter = CGS.R_Jupiter * 1e-2  # meters
+    c = scic  # m/s
+    q = scic.e  # Coulombs
--- tests/test_constants.py
+++ tests/test_constants.py
@@ -10,6 +10,11 @@ class TestConstants(unittest.TestCase):
         self.assertTrue(hasattr(c, 'B0_Jupiter'))
         self.assertTrue(hasattr(c, 'T0'))
         self.assertTrue(hasattr(c, 'T1'))
+        self.assertTrue(hasattr(c, 'R_Earth'))
+        self.assertTrue(hasattr(c, 'R_Saturn'))
+        self.assertTrue(hasattr(c, 'R_Jupiter'))
+        self.assertTrue(hasattr(c, 'c'))
+        self.assertTrue(hasattr(c, 'q'))
 
     def test02_MC2(self):
         """ Test MC2"""
@@ -34,6 +39,26 @@ class TestConstants(unittest.TestCase):
     def test04_T1(self):
         """ Test T1"""
         self.assertAlmostEqual(c.T1, 0.7405, 4)
+
+    def test05_R_Earth(self):
+        """ Test R_Earth cm"""
+        self.assertAlmostEqual(c.R_Earth, 12756/2*1e5, delta=1e6)
+
+    def test05_R_Saturn(self):
+        """ Test R_Saturn cm"""
+        self.assertAlmostEqual(c.R_Saturn, 120536/2*1e5, delta=1e7)
+
+    def test05_R_Jupiter(self):
+        """ Test R_Jupiter cm"""
+        self.assertAlmostEqual(c.R_Jupiter, 142984/2*1e5, delta=1e7)
+
+    def test06_c(self):
+        """ Test speed of light in cm/s"""
+        self.assertAlmostEqual(c.c, 2.9e10, delta=1e9)
+
+    def test06_q(self):
+        """ Test charge in CGS"""
+        self.assertAlmostEqual(c.q, 4.8e-10, delta=1e-10)
 
 
 if __name__ == '__main__':
