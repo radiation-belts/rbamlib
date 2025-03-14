8f047d37976d3a9bdcec41574f0fdfdab55a7b52
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2025-03-14 12:12:06 -0700

    Minor documentation update

--- rbamlib/models/__init__.py
+++ rbamlib/models/__init__.py
@@ -4,7 +4,7 @@ The `models` package is a collection of scientific models for space physics rese
 Contains collection of empirical models of radial diffusion coefficient, lifetimes and local diffusion coefficients scaling, plasma densities, etc.
 
 Packages:
-    - `lpp`: Plasmapause location models
+    - 'lpp': Plasmapause location models
     - 'mp' : Magnetopause location models
     - 'dxx' : Diffusion coefficient scaling models
     - 'dll' : Radial diffusion models
