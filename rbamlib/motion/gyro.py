807c54154597a37f359c8c0c9ff1f587768468bf
Alexander Y Drozdov <alexander.y.drozdov@aero.org>
2025-12-08 15:12:31 -0800

    Updating gyro docstring and L dependence

--- rbamlib/motion/gyro.py
+++ rbamlib/motion/gyro.py
@@ -1,42 +1,58 @@
 import numpy as np
 import rbamlib.constants
 from rbamlib.conv import en2gamma
+from rbamlib.models.dip import B0
 
-def f_gyro(B, en=None, q=rbamlib.constants.q, m=rbamlib.constants.me):
+def f_gyro(B=None, en=None, L=None, planet='Earth', q=rbamlib.constants.q, m=rbamlib.constants.me):
     r"""
-    Calculate the oscillation gyro (cyclotron) frequency.
+    Calculate the oscillation gyro (cyclotron) frequency for a charged particle.
+    
+    This function computes the gyro frequency. It accounts for the relativistic correction when the 
+    particle's kinetic energy is provided.
 
     Parameters
     ----------
-    B : float or ndarray
-        Magnetic field strength in Gauss.
+    B : float or ndarray, optional
+        Magnetic field strength, in Gauss. Either B or L must be provided.
     en : float or ndarray, optional
-        Kinetic energy in MeV. If provided, the relativistic correction is applied.
-    q : float or ndarray, optional
-        Particle charge (SGS charge).
-    m : float or ndarray, optional
-        Particle mass (g).
-    
+        Kinetic energy of the particle, in MeV. When provided, a relativistic correction via 
+        the Lorentz factor is applied.
+    L : float or ndarray, optional
+        McIlwain L-shell parameter (dimensionless). Used to compute B if it is not provided.
+    planet : str, optional, default = 'Earth'
+        Name of the planet for which the magnetic field is calculated.
+    q : float, optional
+        Particle charge, in statcoulombs (SGS units). Defaults is for electrons.
+    m : float, optional
+        Particle mass, in grams. Defaults is for electrons.
+
     Returns
     -------
     float or ndarray
-        Gyro frequency in Hz.
-    
+        Gyro frequency, in Hertz (Hz).
+
     Notes
     -----
     For a non-relativistic particle, the gyro frequency is given by
 
-        f = |q| B / (2π m c)
+    .. math::
+        f = \frac{|q| B}{2 \pi m c}
 
-    For a relativistic particle with kinetic energy (K) the Lorentz factor is
+    For a relativistic particle with kinetic energy (en), the Lorentz factor is
 
-        γ = 1 + K/(mc²)
+    .. math::
+        \gamma = 1 + \frac{m \cdot c^2}
 
     and the gyro frequency becomes
 
-        f = |q| B / (2π γ m c)
-        
-    Here, mc² (rest mass energy) is provided by rbamlib.constants.MC2 and is in MeV.
+    .. math::
+       f = \frac{|q| B}{2 \pi \gamma m c}
+
+
+    References
+    ----------
+    .. [#] Chen, F. F. (1984). Introduction to Plasma Physics and Controlled Fusion, Vol. 1. 
+           Plenum Press.
     """
     # Compute gamma if energy is provided (en is in MeV)
     gamma = 1.0
@@ -51,22 +67,97 @@ def f_gyro(B, en=None, q=rbamlib.constants.q, m=rbamlib.constants.me):
         mc2 = mc2_erg * erg_to_MeV
         gamma = en2gamma(en, mc2)
 
+    if B is None and L is not None:
+        B = B0(L, planet=planet)
+    elif B is None and L is None:
+        raise KeyError("Either magnetic field (B) or McIlwain L should be provided.")
+
+
+
     # Compute f
     f = np.abs(q) * B / (2 * np.pi * gamma * m * rbamlib.constants.c)
 
     return f       
 
 
-def omega_gyro(B, en=None, q=rbamlib.constants.q, m=rbamlib.constants.me):
+def omega_gyro(B=None, en=None, L=None, planet='Earth',  q=rbamlib.constants.q, m=rbamlib.constants.me):
+    r"""
+    Calculate the angular gyro (cyclotron) frequency for a charged particle in a magnetic field.
+
+    Parameters
+    ----------
+    B : float or ndarray, optional
+        Magnetic field strength, in Gauss. Either B or L must be provided.
+    en : float or ndarray, optional
+        Kinetic energy of the particle, in MeV. When provided, a relativistic correction via 
+        the Lorentz factor is applied.
+    L : float or ndarray, optional
+        McIlwain L-shell parameter (dimensionless). Used to compute B if it is not provided.
+    planet : str, optional, default = 'Earth'
+        Name of the planet for which the magnetic field is calculated.
+    q : float, optional
+        Particle charge, in statcoulombs (SGS units). Defaults is for electrons.
+    m : float, optional
+        Particle mass, in grams. Defaults is for electrons.
+
+    Returns
+    -------
+    float or ndarray
+        Angular gyro frequency, in radians per second (rad/s).
+
+    Notes
+    -----
+    The angular gyro frequency is calculated from the standard gyro frequency f by:
+
+    .. math::
+        \omega = 2 \pi f
+
+    References
+    ----------
+    .. [#] Chen, F. F. (1984). Introduction to Plasma Physics and Controlled Fusion, Vol. 1. 
+           Plenum Press.
     """
-        Calculate the angular (ω) gyro (cyclotron) frequency.        
-    """
-    return 2 * np.pi * f_gyro(B, en, q, m)
+    return 2 * np.pi * f_gyro(B=B, en=en, L=L, planet=planet, q=q, m=m)
 
 
-def T_gyro(B, en=None, q=rbamlib.constants.q, m=rbamlib.constants.me):
+def T_gyro(B=None, en=None, L=None, planet='Earth',  q=rbamlib.constants.q, m=rbamlib.constants.me):
+    r"""
+    Calculate the gyro period of a charged particle in a magnetic field.
+
+    Parameters
+    ----------
+    B : float or ndarray, optional
+        Magnetic field strength, in Gauss. Either B or L must be provided.
+    en : float or ndarray, optional
+        Kinetic energy of the particle, in MeV. When provided, a relativistic correction via 
+        the Lorentz factor is applied.
+    L : float or ndarray, optional
+        McIlwain L-shell parameter (dimensionless). Used to compute B if it is not provided.
+    planet : str, optional, default = 'Earth'
+        Name of the planet for which the magnetic field is calculated.
+    q : float, optional
+        Particle charge, in statcoulombs (SGS units). Defaults is for electrons.
+    m : float, optional
+        Particle mass, in grams. Defaults is for electrons.
+    
+    
+    Returns
+    -------
+    float or ndarray
+        Gyro period, in seconds (s).
+
+    Notes
+    -----
+    The gyro period is defined as the inverse of the angular gyro frequency:
+
+        T = \frac{2\pi}{\omega}
+
+    which exactly corresponds to the time taken for one complete gyration of the particle about the magnetic field line.
+
+    References
+    ----------
+    .. [#] Chen, F. F. (1984). Introduction to Plasma Physics and Controlled Fusion, Vol. 1. 
+           Plenum Press.
     """
-        Calculate gyto period        
-    """
-    return 2 * np.pi / omega_gyro(B, en, q, m)
+    return 2 * np.pi / omega_gyro(B=B, en=en, L=L, planet=planet, q=q, m=m)
 
--- tests/test_motion.py
+++ tests/test_motion.py
@@ -2,6 +2,9 @@ import unittest
 import numpy as np
 from rbamlib.models.dip import B
 from rbamlib.motion import f_gyro, omega_gyro, T_gyro
+from rbamlib.motion import f_bounce, omega_bounce, T_bounce
+from rbamlib.motion import f_drift, omega_drift, T_drift
+
 
 class TestUtils(unittest.TestCase):
 
@@ -11,6 +14,7 @@ class TestUtils(unittest.TestCase):
         self.B = np.array([1e4, 0.5e4]) / 1e10 # 1 and 0.5 T in Gauss 
         self.f = np.array([2.8e10, 2.8e10/2]) / 1e10 # Hz
         self.T_gyro = np.array([0.35e-10, 0.71e-10]) * 1e10 # s
+        self.en = np.array([0.01, 0.1, 1])
         # 27990582811, 27990582811/2
 
         self.arr = np.array([1, 2, 3, 4, 5])
@@ -20,24 +24,64 @@ class TestUtils(unittest.TestCase):
         pass
 
     def test_f_gyro(self):
-        result = f_gyro(self.B)
+        result = f_gyro(B=self.B)
         np.testing.assert_array_almost_equal(result, self.f, decimal=1, err_msg="test_f_gyro failes")
 
     def test_omega_gyro(self):
-        result = omega_gyro(self.B)
+        result = omega_gyro(B=self.B)
         np.testing.assert_array_almost_equal(result, 2 * np.pi * self.f , decimal=1, err_msg="test_omega_gyro failed")
 
     def test_T_gyro(self):
-        result = T_gyro(self.B)
+        result = T_gyro(B=self.B)
         np.testing.assert_array_almost_equal(result, self.T_gyro,
                                               decimal=1, err_msg="test_T_gyro failes")
 
         B0 = 0.3**2 # L=2 with B0=0.3
-        result = T_gyro(B0, en=np.array([0.01, 0.1, 1]))
-        # Physics of Earth Radiaion Belts Table
+        result = T_gyro(B=B0, en=self.en)
+        # Physics of Earth Radiaion Belts Table 2.2
         np.testing.assert_array_almost_equal(result, np.array([9.71e-6, 11.4e-6, 28.1e-6]),
                                               decimal=1, err_msg="test_T_gyro with energy failes")
+        
+    def test_T_gyro_L(self):        
+        L = 2 # L=2 
+        result = T_gyro(L=2, en=self.en)
+        # Physics of Earth Radiaion Belts Table 2.2
+        np.testing.assert_array_almost_equal(result, np.array([9.71e-6, 11.4e-6, 28.1e-6]),
+                                              decimal=1, err_msg="test_T_gyro_L with energy failes")
 
+    def test_f_bounce(self):
+        result = f_bounce(L=4, al=np.deg2rad(80), en=self.en[0])
+        np.testing.assert_array_almost_equal(result, 1/1.30,
+                                              decimal=1, err_msg="test_f_bounce failes")
+        
+    def test_omega_bounce(self):
+        result = omega_bounce(L=4, al=np.deg2rad(80), en=self.en[0])        
+        np.testing.assert_array_almost_equal(result, 2 * np.pi / 1.30,
+                                              decimal=1, err_msg="test_omega_bounce failes")
+
+    def test_T_bounce(self):
+        result = T_bounce(L=4, al=np.deg2rad(80), en=self.en)
+        # Physics of Earth Radiaion Belts Table 2.2
+        # The value for 0.01 keV is likely incorrect, it is calculated without relativistic assumtion
+        np.testing.assert_array_almost_equal(result, np.array([1.30, 0.46, 0.27]),
+                                              decimal=2, err_msg="test_T_gyro failes")
+
+    def test_T_drift(self):
+        result = T_drift(L=4, al=np.pi/2, en=np.array([0.001, 0.01])) / 60 / 60 # Convert to h
+        # Quantitatve aspectes of Magnetrophseric Phyusics
+        # Lyons and Williams, table 2.2 (only approximate non relativistic case)
+        np.testing.assert_array_almost_equal(result, np.array([184, 18.4]),
+                                              decimal=0, err_msg="test_T_drift failes")
+
+    def test_omega_drift(self):
+        result = omega_drift(L=4, al=np.pi/2, en=0.01)
+        np.testing.assert_array_almost_equal(result, 1 / (18.4 * 60 * 60) / (2 * np.pi),
+                                              decimal=0, err_msg="test_T_drift failes")
+
+    def test_f_drift(self):
+        result = f_drift(L=4, al=np.pi/2, en=0.01)
+        np.testing.assert_array_almost_equal(result, 1 / (18.4 * 60 * 60),
+                                              decimal=0, err_msg="test_T_drift failes")        
 
 if __name__ == '__main__':
     unittest.main()
